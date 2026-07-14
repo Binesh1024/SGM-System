39e06ab7046eeb765b578cb039c14bff73b715b5
Binesh1024 <shresthabinesh64@gmail.com>
2026-07-14 16:43:50 +0545

    fix:conflict

--- system/models.py
+++ system/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from accounts.models import BaseModel
 
 class Class(BaseModel):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     section = models.CharField(max_length=10)
     academic_year = models.CharField(max_length=20)
     class_code = models.CharField(max_length=10, unique=True, blank=True, null=True)
@@ -18,4 +18,4 @@ class Class(BaseModel):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.name} - {self.section} ({self.academic_year})"
+        return f"{self.name} - {self.section} ({self.academic_year})"   
